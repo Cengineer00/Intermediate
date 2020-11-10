7daa6037420265aa1209eed8e4eb1d6e0723806b
Cengineer00 <57703769+Cengineer00@users.noreply.github.com>
2020-11-10 21:30:41 +0300

    Update pascal_triangle.py
    I added 2 functions, char finder returns the length of a row of pascal triangle in sense of character. Pyramide printer, prints the pyramide. Functions can be improved both for better syntatical look and efficiency.

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,15 +1,50 @@
+import math
+
+
 def pascal(x):
-    base=[[1]]
+    base = [[1]]
     for i in range(x):
         base.append([0])
-    for i in range(x-1):
-        for j in range(i+1):
-            base[i+1].append(0)
-        base[i+1][0]=1
-        base[i+1][-1]=1    
-    base=base[:-1]
-    
-    for i in range(2,x):
-        for j in range(1,i):
-            base[i][j]=base[i-1][j-i]+base[i-1][j-i-1]
+    for i in range(x - 1):
+        for j in range(i + 1):
+            base[i + 1].append(0)
+        base[i + 1][0] = 1
+        base[i + 1][-1] = 1
+    base = base[:-1]
+
+    for i in range(2, x):
+        for j in range(1, i):
+            base[i][j] = base[i - 1][j - i] + base[i - 1][j - i - 1]
+
     return base
+
+def pascal_char_finder(a):
+    list = (pascal(a))[-1]
+
+    result = 0
+
+    for i in list:
+        result += len(str(i))
+
+    result += a*2
+    return result
+
+
+def pascal_pyramide_printer(x):
+    blank = pascal_char_finder(x) +10
+    base = pascal(x+1)
+    blank_left = int(blank / 2 +1)
+    toplam_blank = (blank_left+2)*2 + 3
+
+    for i in range(len(base)-1):
+        blank_right = toplam_blank - math.ceil(blank_left) - pascal_char_finder(i+1) -4
+
+        print("|", int(math.ceil(blank_left)) * " ", base[i], int(blank_right) * " ", "|")
+
+        x = pascal_char_finder(i+1)
+        y = pascal_char_finder(i+2)
+
+        blank_left -= (y-x)/2
+    return 0
+
+#pascal_pyramide_printer(15)
