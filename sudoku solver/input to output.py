1fd324be9eb76b32e54429f7c7ce54136c033a46
Cengineer00 <57703769+Cengineer00@users.noreply.github.com>
2021-01-08 01:47:44 +0300

    Create input to output.py
    Reads the input from the "sudokuinput.txt" files and prints them to the "sudokuoutput.txt" file. Prints the execution time.

---
+++ sudoku solver/input to output.py
@@ -0,0 +1,30 @@
+import SudokuSolver as solver
+import time
+
+start_time = time.time()
+
+f = open("sudokuoutput.txt", "a")
+
+for i in range(4):
+    input_file = open("sudokuinput"+str(i+1)+".txt", "r")
+    nextline = input_file.readline()
+
+    problem = []
+    while (nextline != ""):
+        problem.append([int(num) for num in nextline.split(" ")])
+
+        nextline = input_file.readline()
+
+    input_file.close()
+
+    solution = solver.sudokusolver(problem)
+    for i in range(9):
+        for j in range(9):
+            f.write(str(solution[i][j]) + " ")
+        f.write("\n")
+
+    f.write("\n---------------------------\n")
+
+f.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
