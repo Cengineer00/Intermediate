217ce07752804db7cfaa1fbf05a780cb8aef1f1a
Cengineer00 <kose.orhun@metu.edu.tr>
2021-08-08 17:55:06 +0300

    Change to OOP

--- sudoku solver/solver.py
+++ sudoku solver/solver.py
@@ -1,64 +1,54 @@
-def numbertoindex(n):
-    return [(n//9), n%9]
+from helperfunctions import *
+from copy import copy, deepcopy
 
-def indextonumber(i,j):
-    return i*9+j
+class Sudoku:
 
-def condition(index, problem):
+    def __init__(self, problem):
+        if(type(problem) == str and problem[-3:] == "txt"):
+            input_file = open(problem, "r")
+            nextline = input_file.readline()
 
-    number = problem[index[0]][index[1]]
+            self.problem = []
+            while (nextline != ""):
+                self.problem.append([int(num) for num in nextline.split(" ")])
 
-    square_column = (index[1]//3)*3
-    square_row = (index[0]//3)*3
+                nextline = input_file.readline()
+            self.solution = [row[:] for row in self.problem]
 
-
-    for i in range(3):
-        for j in range(3):
-            if (problem[square_row+i][square_column+j] == number and index != [square_row+i, square_column+j]):
-                return False
-
-    for i in range(9):
-        if(problem[i][index[1]] == number and index[0] != i):
-            return False
-
-    for j in range(9):
-        if(problem[index[0]][j] == number and index[1] != j):
-            return False
-
-    return True
-
-
-def sudokusolver(problem):
-    stack = []
-    stack.append(0)
-    dontvisit = []
-
-    for i in range(9):
-        for j in range(9):
-            if problem[i][j] != 0:
-                dontvisit.append([i, j])
-
-    while (stack != []):
-        index = numbertoindex(stack[-1])
-        while True:
-            if (index in dontvisit):
-                stack[-1] += 1
-                index = numbertoindex(stack[-1])
-                continue
-            break
-        
-        if(stack[-1] == 81): break
-
-        i = problem[index[0]][index[1]]
-        while (i < 10):
-            if (i == 9):
-                problem[index[0]][index[1]] = 0
-                stack.pop()
-                break
+            input_file.close()
+        elif(type(problem) == str):
+            pass
+        else:
+            self.problem = problem
+            self.solution = problem
+    
+    def __str__(self):
+        for i in range(9):
+            for j in range(9):
+                if(j == 2 or j == 5):
+                    print(self.solution[i][j], end= " | ")
+                else:
+                    print(self.solution[i][j], end="  ")
+            if(i == 2 or i == 5):
+                print("")
+                print("----------------------------")
             else:
-                problem[index[0]][index[1]] = i + 1
-                if (condition(index, problem)):
-                    stack.append(stack[-1] + 1)
-                    break
-            i += 1
-    return problem
+                print("")
+
+        return ""
+    
+    def solve(self):
+        problem_copy = [row[:] for row in self.problem]
+        self.solution = sudokusolver(problem_copy)
+
+
+#EXAMPLE
+
+instance = Sudoku("sudokuinput1.txt")
+
+print(instance)
+print("")
+
+instance.solve()
+
+print(instance)
